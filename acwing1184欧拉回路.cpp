391d3ae5d8f01b75acb0db120a81e12d3924be5f
sakuralggm <100845909+sakuralggm@users.noreply.github.com>
2023-10-11 11:56:20 +0800

    Create acwing1184欧拉回路.cpp

---
+++ acwing1184欧拉回路.cpp
@@ -0,0 +1,92 @@
+// #define LOCAL
+#ifdef LOCAL
+#define dbg(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define dbg(...)
+#define NDEBUG
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+const int N = 1e5 + 10, M = 4e5 + 10;
+struct Edge
+{
+    int to, ne;
+    bool used;
+} e[M];
+int h[N], idx = 2;
+int in[N], out[N];
+int stk[M], top;
+int n, m, type;
+
+void add(int a, int b)
+{
+    e[idx].to = b, e[idx].ne = h[a], h[a] = idx ++ ;
+}
+
+void dfs(int u)
+{
+    while (h[u])
+    {
+        int t = h[u];
+        if (e[t].used) 
+        {
+            h[u] = e[t].ne;
+            continue;
+        }
+        e[t].used = true;
+        if (type == 1) e[t ^ 1].used = true;
+        dfs(e[t].to);
+        // 把边的编号加入栈
+        if (type == 1)
+        {
+            if (t & 1) stk[top ++ ] = -(t / 2);
+            else stk[top ++ ] = t / 2;
+        }
+        else stk[top ++ ] = t - 1;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    cin >> type >> n >> m; // type=1无向图， type=2有向图
+    int st = 0;
+    for (int i = 0; i < m; i ++ )
+    {
+        int a, b;
+        cin >> a >> b;
+        st = a;
+        if (type == 1) add(a, b), add(b, a);
+        else add(a, b);
+        in[b] ++, out[a] ++ ;
+    }
+    // 度数条件
+    bool flag = true;
+    for (int i = 1; i <= n; i ++ )
+    {
+        if ((type == 1 && (in[i] + out[i]) & 1) || (type == 2 && (in[i] != out[i])))
+        {
+            flag = false;
+            break;
+        }
+    }
+    if (!flag) cout << "NO";
+    else
+    {
+        dfs(st);
+        if (top != m) flag = false;
+        if (flag)
+        {
+            cout << "YES\n";
+            for (int i = top - 1; i >= 0; i -- )
+                cout << stk[i] << ' ';
+        }
+        else cout << "NO";
+    }
+    return 0;
+}
