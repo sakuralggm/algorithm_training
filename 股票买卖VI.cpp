2f0da8632b13c2b84d8a12560461e4fec8266b9c
sakuralggm <100845909+sakuralggm@users.noreply.github.com>
2023-10-13 00:03:39 +0800

    Create 股票买卖VI.cpp

---
+++ 股票买卖VI.cpp
@@ -0,0 +1,32 @@
+#define LOCAL
+#ifdef LOCAL
+#define dbg(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define dbg(...)
+#define NDEBUG
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+const int N = 1e5 + 10;
+int w[N];
+int f[N][2];
+int main()
+{
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    int n, c;
+    cin >> n >> c;
+    for (int i = 1; i <= n; i ++ ) cin >> w[i];
+    f[0][1] = -0x3f3f3f3f;
+    for (int i = 1; i <= n; i ++ )
+    {
+        f[i][0] = max(f[i - 1][0], f[i - 1][1] + w[i]);
+        f[i][1] = max(f[i - 1][1], f[i - 1][0] - w[i] - c);
+    }
+    cout << f[n][0];
+    return 0;
+}
