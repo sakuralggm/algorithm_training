d4e5c1e1035f86f962808fa434b9378d9eedfb5c
Index2022 <2033541709@qq.com>
2024-06-06 13:29:07 +0800

    upload

--- CSP400/CSP-J/queue/T3/P1886.cpp
+++ CSP400/CSP-J/queue/T3/P1886.cpp
@@ -14,7 +14,7 @@
  *    初始写法：
  *    优化写法：
 **/
-#define LOCAL
+// #define LOCAL
 #ifdef LOCAL
 #define dbg(...) fprintf(stderr, __VA_ARGS__)
 #define debug(x) cerr << #x << ' ' <<  '=' << ' ' << x << endl
@@ -30,9 +30,47 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
+const int N = 1e6 + 10;
+int a[N];
+int q[N], hh, tt;
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
-    
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        while (hh < tt && i - q[hh] >= k) {
+            hh++;
+        }
+        while (hh < tt && a[q[tt - 1]] > a[i]) {
+            tt--;
+        }
+        q[tt++] = i;
+        if (i >= k - 1) {
+            cout << a[q[hh]] << ' ';
+        }
+        for (int i = hh; i < tt; i++) {
+            dbg("%d ", q[i]);
+        }
+        dbg("\n");
+    }
+    cout << "\n";
+    hh = tt = 0;
+    for (int i = 0; i < n; i++) {
+        while (hh < tt && i - q[hh] >= k) {
+            hh++;
+        }
+        while (hh < tt && a[q[tt - 1]] < a[i]) {
+            tt--;
+        }
+        q[tt++] = i;
+        if (i >= k - 1) {
+            cout << a[q[hh]] << ' ';
+        }
+    }
     return 0;
 }
