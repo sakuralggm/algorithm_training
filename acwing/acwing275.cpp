7d1be308fbf7f8868916bd45fbbe350caeb177d2
Index2022 <2033541709@qq.com>
2024-07-21 14:51:14 +0800

    update

--- acwing/acwing275.cpp
+++ acwing/acwing275.cpp
@@ -1,18 +1,18 @@
 // clang-format off
 /**
- *    题目链接：
+ *    题目链接：https://www.acwing.com/problem/content/277/
  *    本题耗时：
- *    是否独立完成：
- *    提交次数：
- *    时间复杂度：
- *    空间复杂度：
- *    知识点：
- *    解题思路：
+ *    是否独立完成：是
+ *    提交次数：2
+ *    时间复杂度：O((m+n) * m * m)
+ *    空间复杂度：O((m+n) * m * m)
+ *    知识点：线性DP，数字三角形模型
+ *    解题思路：一来一回地走，可以看成(1,1)走到(m,n)两次，思路和acwing1027方格取数相同。
  *    技巧：
  *    优化：
  *    错误：
  *    参考题解：
- *    同类题目：
+ *    同类题目：https://www.acwing.com/problem/content/1029/
  *    初始写法：
  *    优化写法：
 **/
@@ -20,7 +20,7 @@
 #define LOCAL
 #ifdef LOCAL
 #define dbg(...) fprintf(stderr, __VA_ARGS__)
-#define debug(x) cerr << #x << ' ' <<  '=' << ' ' << x << endl
+#define debug(x) cerr << #x << ' ' << '=' << ' ' << x << endl
 #else
 #define dbg(...)
 #define debug(x)
@@ -33,10 +33,44 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 
+const int N = 60;
+int w[N][N], f[N * 2][N][N];
+
 int main()
 {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
-    
+    int m, n;
+    cin >> m >> n;
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cin >> w[i][j];
+        }
+    }
+    for (int k = 2; k <= m + n; k++)
+    {
+        for (int i1 = 1; i1 <= m; i1++)
+        {
+            for (int i2 = 1; i2 <= m; i2++)
+            {
+                int j1 = k - i1, j2 = k - i2;
+                if (j1 >= 1 && j1 <= n && j2 >= 1 && j2 <= n)
+                {
+                    int t = w[i1][j1];
+                    if (i1 != i2)
+                    {
+                        t += w[i2][j2];
+                    }
+                    f[k][i1][i2] = max({ f[k - 1][i1][i2], f[k - 1][i1][i2 - 1], f[k - 1][i1 - 1][i2],
+                                         f[k - 1][i1 - 1][i2 - 1] }) +
+                                   t;
+                }
+            }
+        }
+    }
+
+    cout << f[m + n][m][m];
     return 0;
 }
