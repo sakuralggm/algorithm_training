504a6a48546b89dd3f00790496d98efe46095410
Index2022 <2033541709@qq.com>
2024-11-15 11:12:19 +0800

    modified:   bytedance/最大乘积区间问题.py

--- bytedance/最大乘积区间问题.py
+++ bytedance/最大乘积区间问题.py
@@ -1,31 +1,22 @@
 def solution(n, data):
+    max_x, max_y = 1, 1
     max_product = -1
-    max_x = 1
-    max_y = 1
     current_product = 1
-    current_start = 0
+    curr_x, curr_y = 1, 1
     for i in range(n):
         if data[i] == 0:
-            if max_product < 0 or 0 > max_product:
-                max_product = 0
-                max_x = i + 1
-                max_y = i + 1
             current_product = 1
-            current_start = i + 1
+            curr_x = i + 2
+            curr_y = i + 2
         else:
             current_product *= data[i]
+            curr_y = i + 1
             if current_product > max_product:
                 max_product = current_product
-                max_x = current_start + 1
-                max_y = i + 1
-            elif current_product == max_product:
-                if (current_start + 1) < max_x:
-                    max_x = current_start + 1
-                    max_y = i + 1
-                elif (current_start + 1) == max_x and (i + 1) < max_y:
-                    max_y = i + 1
+                max_x = curr_x
+                max_y = curr_y
     return [max_x, max_y]
-
+            
 
 if __name__ == "__main__":
     # Add your test cases here
